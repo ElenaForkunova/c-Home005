6a3f70f0d334ac44923c6bc86bd82beba11dd78e
Elena Forkunova <forkelena@yandex.ru>
2022-11-21 23:10:22 +0400

    домашка 005 задачи 34,36,38

--- Home005_34_36_38/Program.cs
+++ Home005_34_36_38/Program.cs
@@ -3,7 +3,7 @@
 [345, 897, 568, 234] -> 2 */
 
 //инициируем массив:
-/*int[] array = new int [new Random().Next(5,10)];
+int[] array = new int [new Random().Next(5,10)];
 
 for (int i = 0; i < array.Length; i++)
 {
@@ -24,14 +24,15 @@ int Count(int[] array)
     return count;
 }
 
-Console.WriteLine($"количество чётных чисел в массиве = {Count(array)}");*/
+Console.WriteLine($"количество чётных чисел в массиве = {Count(array)}");
+Console.WriteLine($" ");
 
 /*Задача 36: Задайте одномерный массив, заполненный случайными числами. 
 Найдите сумму элементов, стоящих на нечётных позициях.
 [3, 7, 23, 12] -> 19
 [-4, -6, 89, 6] -> 0*/
 
-/*int[] array1 = new int [new Random().Next(4,11)];
+int[] array1 = new int [new Random().Next(4,11)];
 
 for (int i = 0; i < array1.Length; i++)
 {
@@ -44,27 +45,29 @@ for (int i = 1; i < array1.Length; i++)
         if(i%2!=0)
         sum +=array1[i];
     }
-Console.WriteLine("сумма элементов cтоящих на нечётных позициях " + sum ); */
+Console.WriteLine("сумма элементов cтоящих на нечётных позициях " + sum );
+Console.WriteLine($" ");
 
 /*Задача 38: Задайте массив вещественных чисел. 
 Найдите разницу между максимальным и минимальным элементов массива.
 [3 7 22 2 78] -> 76 */
 
-double[] array = new double[10];
+double[] array2 = new double[10];
 Random rand = new Random();
-for (int i = 0; i < array.Length; i++)
+for (int i = 0; i < array2.Length; i++)
 {
-    array[i] = rand.NextDouble() * 100;     
+    array2[i] = rand.NextDouble() * 100;     
     /*100 - rand.Next(100) + rand.NextDouble();*/
-    Console.Write("{0,6:F2}", array[i] + " ");
+    Console.WriteLine("{0,6:F2}", array2[i] + " ");
 }
-var max = array[0];
-var min = array[0];
+var max = array2[0];
+var min = array2[0];
     for (int i = 1; i < 10; i++)
         {
-            if (array[i] < min) min = array[i];
-            if (array[i] > max) max = array[i];
+            if (array2[i] < min) min = array2[i];
+            if (array2[i] > max) max = array2[i];
         }
 
 Console.WriteLine($"Максимальное значение = {max}, минимальное значение = {min}");
 Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
+Console.WriteLine($" ");
